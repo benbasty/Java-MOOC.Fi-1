235ed991032cd99e8ecede1da2967a12fc3a9d79
benbasty <benbasty@yahoo.fr>
2021-06-25 15:08:50 +0800

    Solutions Mooc.Fi - Java Programming I - Part 6

---
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -0,0 +1,37 @@
+
+import java.util.ArrayList;
+
+public class SimpleCollection {
+
+    private String name;
+    private ArrayList<String> elements;
+
+    public SimpleCollection(String name) {
+        this.name = name;
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(String element) {
+        this.elements.add(element);
+    }
+
+    public ArrayList<String> getElements() {
+        return this.elements;
+    }
+    public String toString(){
+        String numberOfElementsString = "";
+        
+        if(this.elements.isEmpty()) {
+            return "The collection " + this.name + " is empty.";
+        } else if(this.elements.size() == 1) {
+            return "The collection " + this.name + " has " + this.elements.size() + " element:\n"+ this.elements.get(0);
+        } else {
+           for(String element: elements) {
+               numberOfElementsString = numberOfElementsString + element + "\n";
+           }
+           return "The collection " + this.name + " has " + elements.size() + " elements:\n"+ numberOfElementsString;
+        }
+     
+    }
+    
+}
